1ac13f0ddca1f3c8ac21407d57dc25db73b6c747
JustCookieDough <scottangelides@gmail.com>
2024-07-11 17:30:55 -0400

    made the sign in page pwetty

--- src/app.py
+++ src/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask import url_for, render_template
+from settings import DISCORD_OAUTH2_PROVIDER_INFO
+from werkzeug.exceptions import HTTPException
 
 app =Flask("__name__")
 
+app.config['OAUTH2_PROVIDER'] = DISCORD_OAUTH2_PROVIDER_INFO
 
 @app.route("/")
 def index():
@@ -13,6 +16,14 @@ def index():
 def login():
     return render_template('login.jinja')
 
+
+@app.route("/auth")
+def authorize():
+   pass
+
+@app.route("/callback")
+def oauth2_callback():
+   pass
 
 @app.route("/bets")
 def bets():
@@ -25,8 +36,8 @@ def leaderboard():
 
 
 # error handler. tosses you to 404 page for pnf errors, a different page for everything else
-@app.errorhandler(Exception) 
-def not_found(e): 
+@app.errorhandler(HTTPException) 
+def not_found(e):
   if (e.code == 404):
     return render_template("404.jinja")
   else:
--- src/settings.py
+++ src/settings.py
@@ -5,4 +5,15 @@ OAUTH_SECRET = config('DISCORD_CLIENT_SECRET')
 
 OAUTH_AUTH_URL = "https://discord.com/oauth2/authorize"
 OAUTH_TOKEN_URL = "https://discord.com/api/oauth2/token"
-OAUTH_TOKEN_REVOKE_URL = "https://discord.com/api/oauth2/token/revoke"
+
+DISCORD_OAUTH2_PROVIDER_INFO = {
+        'client_id': OAUTH_ID,
+        'client_secret': OAUTH_SECRET,
+        'authorize_url': OAUTH_AUTH_URL,
+        'token_url': OAUTH_TOKEN_URL,
+        'userinfo-req': {
+            'url': 'https://discord.com/api/users/@me',
+            'user': lambda json: json['username'],
+        },
+        'scopes': ['identify'],
+}
